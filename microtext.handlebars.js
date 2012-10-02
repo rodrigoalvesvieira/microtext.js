1b3b026e9e6c8035994aab133cf12382960b5ea3
Rodrigo Alves Vieira <rodrigovieira1994@gmail.com>
2012-10-02 15:41:26 -0300

    Adding the newest helper, capitalize, to the Handlebars.js plugin. Also making the Handlebars.js plugin JSLint complete.

--- microtext.handlebars.js
+++ microtext.handlebars.js
@@ -6,41 +6,54 @@
  *
 */
 
-Handlebars.registerHelper('breakSentence', function(sentence) {
-  return sentence.trim().split(" ");
+Handlebars.registerHelper('breakSentence', function (sentence) {
+    "use strict";
+    return sentence.trim().split(" ");
 });
 
-Handlebars.registerHelper('truncate', function(sentence) {
-  if (sentence.constructor === String) {
-    return new Handlebars.SafeString(sentence.slice(0, 10) + "...");
-  }
+Handlebars.registerHelper('truncate', function (sentence) {
+    "use strict";
+    if (sentence.constructor === String) {
+        return new Handlebars.SafeString(sentence.slice(0, 10) + "...");
+    }
 });
 
-Handlebars.registerHelper('firstName', function(names) {
-  var brokenNames = Handlebars.helpers.breakSentence(names);
-  return brokenNames[0];
+Handlebars.registerHelper('firstName', function (names) {
+    "use strict";
+    var brokenNames = Handlebars.helpers.breakSentence(names);
+    return brokenNames[0];
 });
 
-Handlebars.registerHelper('lastName', function(names) {
-  var brokenNames = Handlebars.helpers.breakSentence(names);
-  if (brokenNames.length > 1) {
-    return brokenNames[brokenNames.length -1];
-  }
-  else if (brokenNames.length === 1) {
-    return Handlebars.helpers.firstName(names);
-  }
+Handlebars.registerHelper('lastName', function (names) {
+    "use strict";
+    var brokenNames = Handlebars.helpers.breakSentence(names);
+    if (brokenNames.length > 1) {
+        return brokenNames[brokenNames.length - 1];
+    }
+
+    if (brokenNames.length === 1) {
+        return Handlebars.helpers.firstName(names);
+    }
 });
 
-Handlebars.registerHelper('getInitials', function(expr) {
-  var brokenNames = Handlebars.helpers.breakSentence(expr),
-      nameList = [];
+Handlebars.registerHelper('getInitials', function (expr) {
+    "use strict";
+    var brokenNames = Handlebars.helpers.breakSentence(expr),
+        abbrevName,
+        nameList = [];
 
-  brokenNames.forEach(function(name) {
-    nameList.push(name.charAt(0));
-  });
+    brokenNames.forEach(function (name) {
+        nameList.push(name.charAt(0));
+    });
 
-  if (nameList.length > 1) {
-    var abbrevName = nameList.toString().toUpperCase();
-    return abbrevName.replace(/\,/g, ". ");
-  }
+    if (nameList.length > 1) {
+        abbrevName = nameList.toString().toUpperCase();
+        return abbrevName.replace(/\,/g, ". ");
+    }
+});
+
+Handlebars.registerHelper('capitalize', function (expr) {
+    "use strict";
+    var capitalizedExpr = expr.charAt(0).toUpperCase() + expr.slice(1);
+    return capitalizedExpr;
 });
